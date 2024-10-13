1aa6a9975c1188aca5049ad102fa00799561ef5c
DavidSenn <178643637+DavidStaus@users.noreply.github.com>
2024-10-13 22:17:42 +0200

    Add reset button to draw new custom grid

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16*16; i++) {
-    let smallDiv = document.createElement('div');
-    smallDiv.className = "square";
-    container.appendChild(smallDiv);
-}
+newGrid(16);
 
-const squares = document.querySelectorAll('.square');
-
-squares.forEach( (sqr) => {
+function newGrid(num) {
+    if (num > 100) {
+        alert('Too big! Maximum size: 100x100');
+        return;
+    }
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    for (let i = 0; i < num; i++) {
+        let row = document.createElement('div');
+        row.className = 'row';
+        for (let j = 0; j < num; j++) {
+            let smallDiv = document.createElement('div');
+            smallDiv.className = "square";
+            row.appendChild(smallDiv);
+        }
+        container.appendChild(row);
+    }
+    
+    const squares = document.querySelectorAll('.square');
+    
+    squares.forEach( (sqr) => {
     sqr.addEventListener('mouseover', () => {
         sqr.style.backgroundColor = 'black';
         console.log('fired!')
     })
 })
-    
+}
+
+
+
+const newDraw = document.querySelector('#newDrawing');
+newDraw.addEventListener('click', () => {
+    newGrid(prompt('How many squares per side?', 16));
+})
